8c527b16004294a8052644598faee8f7d18d0018
uolter <walter.traspadini@pagopa.it>
2022-03-07 11:31:56 +0100

    assigned backend for the init project

---
+++ src/init/env/uat/backend.tfvars
@@ -0,0 +1,4 @@
+bucket         = "terraform-backend-4337"
+key            = "uat/init/tfstate"
+region         = "eu-south-1"
+dynamodb_table = "terraform-lock"
--- src/init/main.tf
+++ src/init/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_version = "~> 1.1.0"
+
+  backend "s3" {}
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
