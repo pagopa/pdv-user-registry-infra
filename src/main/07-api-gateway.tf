713a33f539d57678cce41f5cbd5c1b34e5199d8d
uolter <walter.traspadini@pagopa.it>
2022-03-09 15:55:05 +0100

    api defined via open api spec

--- src/main/07-api-gateway.tf
+++ src/main/07-api-gateway.tf
@@ -14,6 +14,43 @@ resource "aws_api_gateway_vpc_link" "apigw" {
 resource "aws_api_gateway_rest_api" "main" {
   name = format("%s-apigw", local.project)
 
+  body = jsonencode({
+    openapi = "3.0.1"
+    info = {
+      title   = "example"
+      version = "1.0"
+    }
+    paths = {
+      "/" = {
+        get = {
+          x-amazon-apigateway-integration = {
+            httpMethod           = "GET"
+            payloadFormatVersion = "1.0"
+            type                 = "HTTP_PROXY"
+            uri                  = format("http://%s/", module.nlb.lb_dns_name)
+            connectionType       = "VPC_LINK"
+            connectionId         = aws_api_gateway_vpc_link.apigw.id
+            responses = {
+              "200" = {
+                statusCode = "200"
+                responseTemplates = {
+                  "application/json" = "#set ($root=$input.path('$')) { \"stage\": \"$root.name\", \"user-id\": \"$root.key\" }"
+                  "application/xml"  = "#set ($root=$input.path('$')) <stage>$root.name</stage> "
+                }
+              }
+              "302" = {
+                statusCode = "302"
+                responseParameters = {
+                  "method.response.header.Location" = "integration.response.body.redirect.url"
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  })
+
   endpoint_configuration {
     types = ["REGIONAL"]
   }
@@ -21,82 +58,19 @@ resource "aws_api_gateway_rest_api" "main" {
   tags = var.tags
 }
 
-resource "aws_api_gateway_resource" "main" {
-  rest_api_id = aws_api_gateway_rest_api.main.id
-  parent_id   = aws_api_gateway_rest_api.main.root_resource_id
-  path_part   = "getall"
-}
-
-resource "aws_api_gateway_method" "main" {
-  rest_api_id      = aws_api_gateway_rest_api.main.id
-  resource_id      = aws_api_gateway_resource.main.id
-  http_method      = "ANY"
-  authorization    = "NONE"
-  api_key_required = false
-  request_parameters = {
-    "method.request.path.proxy" = true
-  }
-
-}
-
-resource "aws_api_gateway_integration" "main" {
-  rest_api_id = aws_api_gateway_rest_api.main.id
-  resource_id = aws_api_gateway_resource.main.id
-  http_method = aws_api_gateway_method.main.http_method
-
-  type                    = "HTTP_PROXY"
-  integration_http_method = "ANY"
-  uri                     = format("http://%s/{proxy}", module.nlb.lb_dns_name)
-  connection_type         = "VPC_LINK"
-  connection_id           = aws_api_gateway_vpc_link.apigw.id
-  timeout_milliseconds    = 29000 # 50-29000
-
-  cache_key_parameters = ["method.request.path.proxy"]
-  request_parameters = {
-    "integration.request.path.proxy" = "method.request.path.proxy"
-  }
-
-}
-
-resource "aws_api_gateway_method_response" "main" {
-  rest_api_id = aws_api_gateway_rest_api.main.id
-  resource_id = aws_api_gateway_resource.main.id
-  http_method = aws_api_gateway_method.main.http_method
-  status_code = "200"
-}
-
-resource "aws_api_gateway_integration_response" "main" {
-  rest_api_id = aws_api_gateway_rest_api.main.id
-  resource_id = aws_api_gateway_resource.main.id
-  http_method = aws_api_gateway_method.main.http_method
-  status_code = aws_api_gateway_method_response.main.status_code
-
-  response_templates = {
-    "application/json" = ""
-  }
-}
-
 resource "aws_api_gateway_deployment" "main" {
-  depends_on  = ["aws_api_gateway_integration.main"]
   rest_api_id = aws_api_gateway_rest_api.main.id
   stage_name  = "v1"
 }
 
+
+
 /*
-resource "aws_api_gateway_base_path_mapping" "main" {
-  api_id      = aws_api_gateway_rest_api.main.id
-  stage_name  = aws_api_gateway_deployment.main.stage_name
-  domain_name = aws_api_gateway_domain_name.main.domain_name
-}
-
-
 //The API Gateway endpoint
 output "api_gateway_endpoint" {
   value = format("https://", aws_api_gateway_domain_name.main.domain_name)
 }
-
 */
-
 output "invoke_url" {
   value = aws_api_gateway_deployment.main.invoke_url
 }
