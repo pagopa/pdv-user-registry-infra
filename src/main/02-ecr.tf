e819754ef0f91d0b2dbd7d8e912b51109d9908dc
uolter <walter.traspadini@pagopa.it>
2022-03-16 11:33:56 +0100

    container registry trfl

--- src/main/02-ecr.tf
+++ src/main/02-ecr.tf
@@ -7,3 +7,22 @@ resource "aws_ecr_repository" "ecr" {
   tags = { "Name" : format("%s-ecr", local.project) }
 
 }
+
+resource "aws_ecr_lifecycle_policy" "ecr" {
+  repository = aws_ecr_repository.ecr.name
+
+  policy = jsonencode({
+    rules = [{
+      rulePriority = 1
+      description  = "Keeps last 10 images"
+      action = {
+        type = "expire"
+      }
+      selection = {
+        tagStatus   = "any"
+        countType   = "imageCountMoreThan"
+        countNumber = 10
+      }
+    }]
+  })
+}
