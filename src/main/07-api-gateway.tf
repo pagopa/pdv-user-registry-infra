65b9140d9c3b6220d0cb65379a307fd8c83712d8
uolter <walter.traspadini@pagopa.it>
2022-03-29 13:09:02 +0200

    refactor api and open api spec

--- src/main/07-api-gateway.tf
+++ src/main/07-api-gateway.tf
@@ -11,117 +11,6 @@ resource "aws_api_gateway_vpc_link" "apigw" {
 
 }
 
-resource "aws_api_gateway_rest_api" "main" {
-  name           = format("%s-apigw", local.project)
-  api_key_source = "HEADER"
-
-  body = templatefile("./api/ms_tokenizer/api-docs.tpl.json",
-    {
-      uri           = format("http://%s/", module.nlb.lb_dns_name),
-      connection_id = aws_api_gateway_vpc_link.apigw.id
-    }
-  )
-
-  endpoint_configuration {
-    types = ["REGIONAL"]
-  }
-
-  tags = { Name = format("%s-apigw", local.project) }
-}
-
-resource "aws_api_gateway_deployment" "main" {
-  rest_api_id = aws_api_gateway_rest_api.main.id
-  #stage_name  = "prod"
-}
-
-locals {
-  stage_name = "v1"
-}
-
-resource "aws_cloudwatch_log_group" "apigw" {
-  name              = "API-Gateway-Execution-Logs_${aws_api_gateway_rest_api.main.id}/${local.stage_name}"
-  retention_in_days = 7
-}
-resource "aws_api_gateway_stage" "main" {
-  deployment_id = aws_api_gateway_deployment.main.id
-  rest_api_id   = aws_api_gateway_rest_api.main.id
-  stage_name    = local.stage_name
-
-  /*
-  access_log_settings {
-    destination_arn = aws_cloudwatch_log_group.apigw.arn
-    #todo: find a better way to represent this log format.
-    format = "{ \"requestId\":\"$context.requestId\", \"extendedRequestId\":\"$context.extendedRequestId\", \"ip\": \"$context.identity.sourceIp\", \"caller\":\"$context.identity.caller\", \"user\":\"$context.identity.user\", \"requestTime\":\"$context.requestTime\", \"httpMethod\":\"$context.httpMethod\", \"resourcePath\":\"$context.resourcePath\", \"status\":\"$context.status\", \"protocol\":\"$context.protocol\", \"responseLength\":\"$context.responseLength\"}"
-  }
-  */
-}
-
-resource "aws_api_gateway_method_settings" "general_settings" {
-  rest_api_id = aws_api_gateway_rest_api.main.id
-  stage_name  = local.stage_name
-  method_path = "*/*"
-
-  settings {
-    # Enable CloudWatch logging and metrics
-    metrics_enabled    = true
-    data_trace_enabled = true
-    logging_level      = "ERROR"
-    #todo.
-    # Limit the rate of calls to prevent abuse and unwanted charges
-    #throttling_rate_limit  = 100
-    #throttling_burst_limit = 50
-  }
-}
-
-resource "aws_api_gateway_usage_plan" "prod_io" {
-  name        = format("%s-api-plan", local.project)
-  description = "Usage plan for main api"
-
-  api_stages {
-    api_id = aws_api_gateway_rest_api.main.id
-    stage  = aws_api_gateway_stage.main.stage_name
-  }
-
-  /*
-  quota_settings {
-    limit  = 20
-    offset = 2
-    period = "WEEK"
-  }
-  */
-
-  #TODO: tune this settings
-  throttle_settings {
-    burst_limit = 5
-    rate_limit  = 10
-  }
-}
-
-resource "aws_api_gateway_api_key" "prod_io" {
-  name = "prod-io"
-}
-
-resource "aws_api_gateway_usage_plan_key" "prod_io" {
-  key_id        = aws_api_gateway_api_key.prod_io.id
-  key_type      = "API_KEY"
-  usage_plan_id = aws_api_gateway_usage_plan.prod_io.id
-}
-
-output "prod_io_key" {
-  value     = aws_api_gateway_usage_plan_key.prod_io.value
-  sensitive = true
-}
-
-/*
-//The API Gateway endpoint
-output "api_gateway_endpoint" {
-  value = format("https://", aws_api_gateway_domain_name.main.domain_name)
-}
-*/
-output "invoke_url" {
-  value = aws_api_gateway_deployment.main.invoke_url
-}
-
 ## Firewall regional web acl  
 resource "aws_wafv2_web_acl" "main" {
   name        = format("%s-webacl", local.project)
@@ -138,11 +27,6 @@ resource "aws_wafv2_web_acl" "main" {
   }
 
   tags = { Name = format("%s-webacl", local.project) }
-}
-
-resource "aws_wafv2_web_acl_association" "main" {
-  web_acl_arn  = aws_wafv2_web_acl.main.arn
-  resource_arn = "arn:aws:apigateway:${var.aws_region}::/restapis/${aws_api_gateway_rest_api.main.id}/stages/${aws_api_gateway_stage.main.stage_name}"
 }
 
 ## API Gateway cloud watch logs
