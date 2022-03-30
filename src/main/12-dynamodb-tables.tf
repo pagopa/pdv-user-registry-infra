e4ac9801db23db43f121cc317c97eff024a61e90
Walter Traspadini <walter.traspadini@pagopa.it>
2022-03-30 15:59:20 +0200

    Dynamodb table replica for production only (#27)
    * dynamo db table replica in production
    
    * update readme file
    
    * enable stream for global teable

--- src/main/12-dynamodb-tables.tf
+++ src/main/12-dynamodb-tables.tf
@@ -6,9 +6,10 @@ locals {
 module "dynamodb_table" {
   source = "terraform-aws-modules/dynamodb-table/aws"
 
-  name      = local.dynamodb_table_tokenizer
-  hash_key  = "PK"
-  range_key = "SK"
+  name           = local.dynamodb_table_tokenizer
+  hash_key       = "PK"
+  range_key      = "SK"
+  stream_enabled = var.env_short == "p" ? true : false
 
   attributes = [
     {
@@ -37,12 +38,11 @@ module "dynamodb_table" {
   server_side_encryption_enabled     = true
   server_side_encryption_kms_key_arn = aws_kms_alias.dynamo_db.target_key_arn
 
-  /* TODO --> add replication region in production only.
-  replica_regions = [{
+  replica_regions = var.env_short == "p" ? [{
     region_name = "eu-central-1"
-    kms_key_arn = aws_kms_alias.dynamo_db.target_key_arn
-  }]
-  */
+    kms_key_arn = aws_kms_alias.dynamo_db_replica[0].target_key_arn
+  }] : []
+
 
   tags = { Name = local.dynamodb_table_tokenizer }
 }
