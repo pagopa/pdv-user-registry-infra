d10b9f5fb9124d79ebe3c2af2e80230ad638dc6b
Walter Traspadini <walter.traspadini@pagopa.it>
2022-04-13 18:07:47 +0200

    Dynamodb tables autoscaling (#51)
    * table token autoscaling
    
    * autoscaling table Person

--- src/main/12-dynamodb-token.tf
+++ src/main/12-dynamodb-token.tf
@@ -1,7 +1,7 @@
 locals {
   # Token
   dynamodb_table_token  = "Token"
-  dynamo_gsi_token_name = "gsi_token"
+  dynamo_gsi_token_name = keys(var.table_token_autoscling_indexes)[0]
 }
 
 # Table Token
@@ -13,6 +13,10 @@ module "dynamodb_table_token" {
   range_key                      = "SK"
   stream_enabled                 = var.env_short == "p" ? true : false
   point_in_time_recovery_enabled = var.dynamodb_point_in_time_recovery_enabled
+  billing_mode                   = "PROVISIONED"
+  autoscaling_enabled            = true
+  read_capacity                  = var.table_token_read_capacity
+  write_capacity                 = var.table_token_write_capacity
 
   attributes = [
     {
@@ -34,12 +38,18 @@ module "dynamodb_table_token" {
       name            = local.dynamo_gsi_token_name
       hash_key        = "token"
       projection_type = "ALL"
+      write_capacity  = var.table_token_autoscling_indexes[local.dynamo_gsi_token_name]["write_min_capacity"]
+      read_capacity   = var.table_token_autoscling_indexes[local.dynamo_gsi_token_name]["read_min_capacity"]
     }
   ]
 
 
   server_side_encryption_enabled     = true
   server_side_encryption_kms_key_arn = aws_kms_alias.dynamo_db.target_key_arn
+
+  autoscaling_read    = var.table_token_autoscaling_read
+  autoscaling_write   = var.table_token_autoscaling_write
+  autoscaling_indexes = var.table_token_autoscling_indexes
 
   replica_regions = var.dynamodb_region_replication_enable ? [{
     region_name = "eu-central-1"
