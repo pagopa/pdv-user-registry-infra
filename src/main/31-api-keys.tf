a5bf5c20744e50a49cee12687cfd5aff3c39b7e6
uolter <walter.traspadini@pagopa.it>
2022-04-19 14:08:29 +0200

    removed local variable no more required

--- src/main/31-api-keys.tf
+++ src/main/31-api-keys.tf
@@ -1,7 +1,3 @@
-locals {
-  api_key_list = toset(concat(var.api_keys_tokenizer, var.api_keys_user_registry))
-}
-
 resource "aws_api_gateway_api_key" "tokenizer" {
   for_each = toset(var.api_keys_tokenizer)
   name     = each.value
