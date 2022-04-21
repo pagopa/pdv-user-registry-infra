a8af388dac3dfccee9ab800a39062a0a5243b071
Walter Traspadini <walter.traspadini@pagopa.it>
2022-04-21 09:13:37 +0200

    Iac pipeline prod (#65)
    * update service aws connection for prod
    
    * removed dev param in azure devops pipeline

--- src/azuredevops/04-service_connections.tf
+++ src/azuredevops/04-service_connections.tf
@@ -3,7 +3,7 @@ resource "azuredevops_serviceendpoint_github" "github_ro" {
   project_id            = azuredevops_project.this.id
   service_endpoint_name = "github-ro"
   auth_personal {
-    personal_access_token = jsondecode(data.aws_secretsmanager_secret_version.devops.secret_string)["github_ro_token"]
+    personal_access_token = jsondecode(data.aws_secretsmanager_secret_version.devops_uat.secret_string)["github_ro_token"]
   }
   lifecycle {
     ignore_changes = [description, authorization]
@@ -16,7 +16,7 @@ resource "azuredevops_serviceendpoint_github" "github_pr" {
   project_id            = azuredevops_project.this.id
   service_endpoint_name = "github-pr"
   auth_personal {
-    personal_access_token = jsondecode(data.aws_secretsmanager_secret_version.devops.secret_string)["github_pr_token"]
+    personal_access_token = jsondecode(data.aws_secretsmanager_secret_version.devops_uat.secret_string)["github_pr_token"]
   }
   lifecycle {
     ignore_changes = [description, authorization]
@@ -30,9 +30,9 @@ resource "azuredevops_serviceendpoint_aws" "uat_serviceendpoint" {
   description           = "Managed by AzureDevOps"
 
   access_key_id = jsondecode(
-  data.aws_secretsmanager_secret_version.devops.secret_string)["aws_iac_access_key_id"]
+  data.aws_secretsmanager_secret_version.devops_uat.secret_string)["aws_iac_access_key_id"]
   secret_access_key = jsondecode(
-  data.aws_secretsmanager_secret_version.devops.secret_string)["aws_iac_secret_access_key"]
+  data.aws_secretsmanager_secret_version.devops_uat.secret_string)["aws_iac_secret_access_key"]
 }
 
 resource "azuredevops_serviceendpoint_aws" "prod_serviceendpoint" {
@@ -41,7 +41,7 @@ resource "azuredevops_serviceendpoint_aws" "prod_serviceendpoint" {
   description           = "Managed by AzureDevOps"
 
   access_key_id = jsondecode(
-  data.aws_secretsmanager_secret_version.devops.secret_string)["aws_iac_access_key_id"]
+  data.aws_secretsmanager_secret_version.devops_prod.secret_string)["aws_iac_access_key_id"]
   secret_access_key = jsondecode(
-  data.aws_secretsmanager_secret_version.devops.secret_string)["aws_iac_secret_access_key"]
+  data.aws_secretsmanager_secret_version.devops_prod.secret_string)["aws_iac_secret_access_key"]
 }
