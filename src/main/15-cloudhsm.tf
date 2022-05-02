eb661f34b1f34a23b489cab9b08c8ec81ac9984a
uolter <walter.traspadini@pagopa.it>
2022-05-02 14:48:22 +0200

    vpc owner configuration to create the peering

--- src/main/15-cloudhsm.tf
+++ src/main/15-cloudhsm.tf
@@ -8,13 +8,16 @@ resource "aws_cloudhsm_v2_cluster" "main" {
 
 ## Note: at least two cloudhsm are requird within the cluster to create a 
 ## a key store. 
+/* #TODO ... wait for activation tests
 resource "aws_cloudhsm_v2_hsm" "hsm" {
-  count      = var.create_cloudhsm ? 2 : 0
+  count      = var.create_cloudhsm ? 1 : 0
   subnet_id  = module.vpc.private_subnets[count.index]
   cluster_id = aws_cloudhsm_v2_cluster.main[0].id
 }
 
 data "aws_network_interface" "hsm" {
-  count = var.create_cloudhsm ? 2 : 0
+  count = var.create_cloudhsm ? 1 : 0
   id    = aws_cloudhsm_v2_hsm.hsm[count.index].hsm_eni_id
 }
+
+*/
