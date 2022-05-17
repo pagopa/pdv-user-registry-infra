bd9c04404f8c61abecbf420546ceb9a40c869b9a
Walter Traspadini <walter.traspadini@pagopa.it>
2022-05-17 11:18:21 +0200

    removed task healthcheck (#94)

--- src/main/07-task-user-registry.tf
+++ src/main/07-task-user-registry.tf
@@ -71,15 +71,6 @@ resource "aws_ecs_task_definition" "user_registry" {
         "value": "yyyy-MM-dd HH:mm:ss.SSSZZ"
       }
     ],
-    "healthCheck": {
-        "command": [
-          "CMD-SHELL",
-          "echo hello"
-        ],
-        "interval": 5,
-        "timeout": 5,
-        "retries": 3
-    },
     "cpu": 256,
     "memory": 512,
     "networkMode": "awsvpc"
