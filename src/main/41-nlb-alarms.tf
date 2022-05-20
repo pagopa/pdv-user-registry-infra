098ebd00c3583b8f41c9886922b389cdc34e3dc7
uolter <walter.traspadini@pagopa.it>
2022-05-20 11:16:50 +0200

    fix indix target group

--- src/main/41-nlb-alarms.tf
+++ src/main/41-nlb-alarms.tf
@@ -19,8 +19,8 @@ module "nlb_unhealthy_unhealthy_targets_alarm" {
 
   dimensions = {
     "target" = {
-      TargetGroup = module.nlb[count.index].target_group_names
-      LoadBalacer = module.nlb[count.index].arn_suffix
+      TargetGroup = module.nlb[0].target_group_names[count.index]
+      LoadBalacer = module.nlb[0].arn_suffix
     },
   }
 
