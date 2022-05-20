de9b4e2f89008f4d850381c1c2c589f91500c77b
uolter <walter.traspadini@pagopa.it>
2022-05-20 11:12:54 +0200

    network load balancer alarms

---
+++ src/main/41-nlb-alarms.tf
@@ -0,0 +1,28 @@
+module "nlb_unhealthy_unhealthy_targets_alarm" {
+  source  = "terraform-aws-modules/cloudwatch/aws//modules/metric-alarms-by-multiple-dimensions"
+  version = "~> 3.0"
+
+  count = length(module.nlb.target_group_names)
+
+  alarm_name          = "nlb-unhealthy-unhealthy-target-"
+  actions_enabled     = var.env_short == "p" ? true : false
+  alarm_description   = "Alarm when an unhealthy count is greater than one in the target"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  threshold           = 1
+  period              = 60
+  unit                = "Count"
+
+  namespace   = "AWS/NetworkELB"
+  metric_name = "UnHealthyHostCount"
+  statistic   = "Sum"
+
+  dimensions = {
+    "target" = {
+      TargetGroup = module.nlb[count.index].target_group_names
+      LoadBalacer = module.nlb[count.index].arn_suffix
+    },
+  }
+
+  alarm_actions = [aws_sns_topic.alarms.arn]
+}
