6b2a998c7808ac89fcf687cb4d48073ed45bc4b2
uolter <walter.traspadini@pagopa.it>
2022-06-05 13:04:40 +0200

    change query to show the plan id

--- src/main/cloudwatch-query/apigw-429.sql
+++ src/main/cloudwatch-query/apigw-429.sql
@@ -1,4 +1,5 @@
 fields @timestamp, @message
-| filter @message like 'Method completed with status: 429'
+| parse @message "Plan ID *." as plan
+| filter @message like 'exceeded throttle limit'
 | sort @timestamp desc
 | limit 20
