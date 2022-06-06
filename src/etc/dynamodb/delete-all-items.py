a2f5b08ff7516ef2fec29a5fbcb1ba7e4c0574f8
uolter <walter.traspadini@pagopa.it>
2022-06-06 13:00:52 +0200

    python script to delete all entries in Person table

---
+++ src/etc/dynamodb/delete-all-items.py
@@ -0,0 +1,33 @@
+from time import sleep
+import boto3
+import botocore
+
+TABLE = "Person"
+ID    = "PK"
+
+table = boto3.resource('dynamodb').Table(TABLE)
+stop = False
+count = 0
+  
+while not stop:
+    scan = table.scan()
+    with table.batch_writer() as batch:
+        
+        try:
+            for item in scan['Items']:
+                # print("Item ", item)
+                if count % 500 == 0:
+                    print(count)
+                # print("Item ", item)
+                key = {ID: item[ID], "SK": item["SK"]}
+                
+                batch.delete_item(Key = key)
+                
+                count = count + 1
+            
+        except Exception as e:
+            print(e)
+            stop = True
+        
+        print("Sleep")
+        sleep(20)
--- src/main/.terraform.lock.hcl
+++ src/main/.terraform.lock.hcl
@@ -25,6 +25,7 @@ provider "registry.terraform.io/hashicorp/aws" {
 provider "registry.terraform.io/hashicorp/null" {
   version = "3.1.1"
   hashes = [
+    "h1:71sNUDvmiJcijsvfXpiLCz0lXIBSsEJjMxljt7hxMhw=",
     "h1:Pctug/s/2Hg5FJqjYcTM0kPyx3AoYK1MpRWO0T9V2ns=",
     "zh:063466f41f1d9fd0dd93722840c1314f046d8760b1812fa67c34de0afcba5597",
     "zh:08c058e367de6debdad35fc24d97131c7cf75103baec8279aba3506a08b53faf",
