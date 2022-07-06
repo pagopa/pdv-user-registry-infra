fb1aab7fb55abe0bb88dd9d8a8b2da65d212e973
uolter <walter.traspadini@pagopa.it>
2022-07-06 12:38:24 +0200

    new assume role for sentinel account

---
+++ src/main/60-sentinel.tf
@@ -0,0 +1,23 @@
+# this module set up all resources needed to create resources to send logs to azure sentinel
+resource "aws_iam_role" "sentinel" {
+  count = var.sentinel_servcie_account_id != null ? 1 : 0
+  name  = "MicrosoftSentinelRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        "Principal" : {
+          "AWS" : "${var.sentinel_servcie_account_id}"
+        },
+        "Condition" : {
+          "StringEquals" : {
+            "sts:ExternalId" : "${var.sentinel_workspace_id}"
+          }
+        }
+      }
+    ]
+  })
+}
