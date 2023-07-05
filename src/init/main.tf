619711cf0b19204c567130c2b186a467abb00a7e
uolter <walter.traspadini@pagopa.it>
2023-07-05 12:15:48 +0200

    fix github thumbprint

--- src/init/main.tf
+++ src/init/main.tf
@@ -96,7 +96,8 @@ resource "aws_iam_openid_connect_provider" "github" {
   ]
 
   thumbprint_list = [
-    "6938fd4d98bab03faadb97b34396831e3780aea1"
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd",
   ]
 }
 
