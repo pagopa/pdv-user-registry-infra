d11a7644c84f8f5a5ab6db24cdfabc800a8c35d2
uolter <walter.traspadini@pagopa.it>
2023-09-29 11:01:30 +0200

    disable opsgenie 4xx and uat

--- src/main/33-api-user-registry.tf
+++ src/main/33-api-user-registry.tf
@@ -163,7 +163,7 @@ module "api_user_registry_4xx_error_alarm" {
 
   actions_enabled     = true
   alarm_name          = "high-4xx-rate-"
-  alarm_description   = "${local.runbook_title} ${local.runbook_url} Api User registry error rate has exceeded the threshold."
+  alarm_description   = "Api User registry error rate has exceeded the threshold."
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 2
   threshold           = 500
