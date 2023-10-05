167c734ff7635ff74c37c1faf01988b3391dee9e
giuseppe-gangemi <giuseppe.gangemi@pagopa.it>
2023-10-05 15:50:21 +0200

    refactor: update query for new logs pattern

--- src/main/cloudwatch-query/log-by-traceid.sql
+++ src/main/cloudwatch-query/log-by-traceid.sql
@@ -1,6 +1,5 @@
 fields @timestamp, @message
-| parse @message /(?<timestamp>\d{4}\-\d{2}\-\d{2}T\d{2}\:\d{2}\:\d{2}\.\d{3}(\+|\-)\d{4})\s+(?<level>\w+)\s+\[(?<appName>[\w\-]+)\,(?<traceId>[\w\-]+)\,(?<spanId>[\w\-]+)\]\s+(?<pid>\d+)\s+\-\-\-\s+\[(?<thread>[\w\-]+)\]\s+(?<logger>[\w\-\.]+)\s+\:\s+(?<message>.*)/
-| filter level = "DEBUG"
+| parse @message /(?<timestamp>\d{4}\-\d{2}\-\d{2}T\d{2}\:\d{2}\:\d{2}\.\d{3}(\+|\-)\d{4})\s+(?<level>\w+)\s+\[(?<appName>[\w\-]+)\,(?<traceId>[\w\-]+)\,(?<spanId>[\w\-]+)\,(?<xAmznTraceId>[\w\-\:\=\;]+)\]\s+(?<pid>\d+)\s\-\-\-\s+\[(?<thread>[\w\-]+)\]\s+(?<logger>[\w\-\.]+)\s+\:\s+(?<message>.*)/
 | filter traceId = "<here the traceid>"
 | sort timestamp desc
 | limit 50
