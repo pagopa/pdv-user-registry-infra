7483419afa1d3737f60b91ac915c39bcaec74c19
Walter Traspadini <walter.traspadini@pagopa.it>
2025-01-14 11:16:25 +0100

    update 4xx alarm threshold (#230)

--- src/main/33-api-user-registry.tf
+++ src/main/33-api-user-registry.tf
@@ -168,7 +168,7 @@ module "api_user_registry_4xx_error_alarm" {
   alarm_description   = "Api User registry error rate has exceeded the threshold."
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 2
-  threshold           = 8000
+  threshold           = var.alarm_4xx_threshold
   period              = 300
   unit                = "Count"
   datapoints_to_alarm = 1
