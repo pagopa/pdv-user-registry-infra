62f1f9fb58a5a8b3b7079722e4c8cb285e097111
Walter Traspadini <walter.traspadini@pagopa.it>
2025-02-05 16:44:46 +0100

    Update cw query 404 by namespace

--- src/main/cloudwatch-query/log-count-404-user-reg.sql
+++ src/main/cloudwatch-query/log-count-404-user-reg.sql
@@ -1,4 +1,4 @@
 fields @timestamp, @message
-| parse @message /namespace=(?<plan>[\w]*)]/
+| parse @message /namespace=(?<plan>[\w-]+)]/
 | filter @message like "feign.FeignException$NotFound: [404 ] during [GET]" and @message like "namespace="
 | stats count(*) as Tot by plan
